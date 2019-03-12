f2a04ed0d170a3a29a535273d64bb09bb0eedb61
Matteo Albinola <malbinola@sonicle.com>
2019-03-12 14:23:51 +0100

    Add customized tree column

--- src/sencha/sonicle-extensions/src/tree/Column.js
+++ src/sencha/sonicle-extensions/src/tree/Column.js
@@ -13,10 +13,10 @@ Ext.define('Sonicle.tree.Column', {
             '<div class="{parent.childCls} {parent.elbowCls}-img ',
             '{parent.elbowCls}-<tpl if=".">line<tpl else>empty</tpl>" role="presentation"></div>',
         '</tpl>',
-        '<div class="{childCls} {elbowCls}-img {elbowCls}',
+        '<div class="{childCls} {customElbowCls} {elbowCls}-img {elbowCls}', // added customElbowCls
             '<tpl if="isLast">-end</tpl><tpl if="expandable">-plus {expanderCls}</tpl>" role="presentation"></div>',
         '<tpl if="checked !== null">',
-            '<div role="button" {ariaCellCheckboxAttr}',
+            '<div role="button" {ariaCellCheckboxAttr}', // added customCheckboxCls and checkboxStyle
                 ' class="{childCls} {customCheckboxCls} {checkboxCls}<tpl if="checked"> {checkboxCls}-checked</tpl>"',
 				' <tpl if="checkboxStyle">style="{checkboxStyle}"</tpl>></div>',
         '</tpl>',
@@ -34,7 +34,7 @@ Ext.define('Sonicle.tree.Column', {
             '</tpl>',
             ' role="presentation" class="{childCls} {baseIconCls} {customIconCls} ',
             '{baseIconCls}-<tpl if="leaf">leaf<tpl else><tpl if="expanded">parent-expanded<tpl else>parent</tpl></tpl> {iconCls}" ',
-            '<tpl if="icon">style="background-image:url({icon})"/><tpl else>></div></tpl>',
+            '<tpl if="icon">style="background-image:url({icon})"/><tpl else> <tpl if="checkboxStyle">style="{iconStyle}"</tpl>></div></tpl>',
         '</tpl>',
         '<tpl if="href">',
             '<a href="{href}" role="link" target="{hrefTarget}" class="{textCls} {childCls}">{value}</a>',
@@ -45,8 +45,10 @@ Ext.define('Sonicle.tree.Column', {
 	
 	initTemplateRendererData: function(value, metaData, record, rowIdx, colIdx, store, view) {
 		return Ext.apply(this.callParent(arguments), {
+			customElbowCls: metaData.customElbowCls || '',
 			customCheckboxCls: metaData.customCheckboxCls || '',
-			checkboxStyle: metaData.checkboxStyle || ''
+			checkboxStyle: metaData.checkboxStyle || '',
+			iconStyle: metaData.iconStyle || ''
 		});
 	}
 });
