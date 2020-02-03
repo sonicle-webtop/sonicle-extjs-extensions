51479250083ca20cc8a81d17e55684db7a9971d6
Matteo Albinola <malbinola@sonicle.com>
2020-02-03 12:16:58 +0100

    Add new menu-item suitable for handling colored tags

---
+++ src/sencha/sonicle-extensions/src/menu/TagItem.js
@@ -0,0 +1,45 @@
+/*
+ * Sonicle ExtJs UX
+ * Copyright (C) 2020 Sonicle S.r.l.
+ * sonicle@sonicle.com
+ * http://www.sonicle.com
+ */
+Ext.define('Sonicle.menu.TagItem', {
+	extend: 'Ext.menu.CheckItem',
+	alias: 'widget.somenutagitem',
+	
+	config: {
+		color: null
+	},
+	
+	/**
+	 * @cfg {String} [defaultColor=#FFFFFF]
+	 * Color to use as default if color is missing.
+	 */
+	defaultColor: '#FFFFFF',
+	
+	showCheckbox: false,
+	glyph: 'xf02b@FontAwesome',
+	checkedCls: 'x-grid-item-selected',
+	uncheckedCls: '',
+	
+	afterRender: function() {
+		var me = this;
+		me.callParent(arguments);
+		me.applyIconColor(me.color);
+	},
+	
+	updateColor: function(nv, ov) {
+		this.applyIconColor(nv);
+	},
+	
+	applyIconColor: function(color) {
+		var iconEl = this.iconEl;
+		if (iconEl && iconEl.dom) {
+			// Use iconEl.dom.style instead of iconEl.getStyle() in order to avoid uncaught exception:
+			// Uncaught TypeError: Cannot read property '0' of undefined at constructor.getStyle
+			iconEl.dom.style.color = color || this.defaultColor;
+			iconEl.dom.style.opacity = 1;
+		}
+	}
+});
