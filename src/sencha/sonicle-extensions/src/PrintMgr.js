d3c27ab47fc46e9b0f6a0d9d4ecf139150b51466
Matteo Albinola <malbinola@sonicle.com>
2020-05-18 09:23:04 +0200

    Add options for QRCode displaying and printing WT-799

--- src/sencha/sonicle-extensions/src/PrintMgr.js
+++ src/sencha/sonicle-extensions/src/PrintMgr.js
@@ -3,10 +3,22 @@
  * Copyright (C) 2015 Sonicle S.r.l.
  * sonicle@sonicle.com
  * http://www.sonicle.com
+ * 
+ * https://stackoverflow.com/questions/468881/print-div-id-printarea-div-only/15900835#15900835
+ * https://stackoverflow.com/questions/45642831/window-print-position-div-center-of-the-page
+ * https://stackoverflow.com/questions/19798923/printing-text-in-html-in-the-center-of-the-page
  */
 Ext.define('Sonicle.PrintMgr', {
 	singleton: true,
 	
+	/**
+	 * 
+	 * @param {String} printable
+	 * @param {String} type
+	 * @param {Object} opts An object containing configuration.
+	 * @param {top|center|bottom} [opts.verticalAlign]
+	 * @param {left|center|right} [opts.horizontalAlign]
+	 */
 	print: function(printable, type, opts) {
 		var me = this, html = '';
 		opts = opts || {};
@@ -15,40 +27,77 @@ Ext.define('Sonicle.PrintMgr', {
 		}
 		
 		if (type === 'raw') {
+			var vaStyle = me.verticalAlignToCssProp(opts.verticalAlign),
+					haStyle = me.horizontalAlignToCssProp(opts.horizontalAlign);
+			
 			html += '<!DOCTYPE html>';
 			html += '<html><head>';
 			html += '<meta charset="UTF-8">';
 			html += '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">';
-			html += (Ext.isEmpty(opts.title) ? '' : '<title>'+opts.title+'</title>');
+			html += (Ext.isEmpty(opts.title) ? '' : '<title>'+Ext.String.htmlEncode(opts.title)+'</title>');
+			html += '<style media="print">';
+			html += 'html, body { height:100%;width:100%;margin:0;padding:0; }';
+			if (!Ext.isEmpty(vaStyle) || !Ext.isEmpty(haStyle)) {
+				html += '#printable { display:flex;height:100%;' + vaStyle + haStyle + ' }';
+			}
+			html += '</style>';
 			html += '</head><body>';
+			html += '<div id="printable">';
 			html += printable;
+			html += '</div>';
 			html += '</body></html>';
 			me.printRawHtml(html);
 		}
 		//TODO: add other types from print.js lib! :)
 	},
 	
-	printRawHtml: function(html) {
-		var ifId = Ext.id(null, 'so-printer-if-'),
-				ifEl, cw;
-		ifEl = Ext.getBody().appendChild({
-			id: ifId,
-			tag: 'iframe',
-			width: '100%',
-			height: '100%',
-			// This fixes Chrome display bug: see https://bugs.chromium.org/p/chromium/issues/detail?id=735059
-			style: 'visibility:hidden;pointer-events:none;border:none;'
-		});
+	privates: {
+		printRawHtml: function(html) {
+			var ifId = Ext.id(null, 'so-printer-if-'),
+					ifEl, cw;
+			ifEl = Ext.getBody().appendChild({
+				id: ifId,
+				tag: 'iframe',
+				width: '100%',
+				height: '100%',
+				// This fixes Chrome display bug: see https://bugs.chromium.org/p/chromium/issues/detail?id=735059
+				style: 'visibility:hidden;pointer-events:none;border:none;'
+			});
+
+			cw = ifEl.dom.contentWindow;
+			cw.document.open();
+			cw.document.write(html);
+			cw.document.close();
+
+			Ext.defer(function() {
+				cw.focus();
+				cw.print();
+				Ext.fly(ifId).destroy();
+			}, 500);
+		},
 		
-		cw = ifEl.dom.contentWindow;
-		cw.document.open();
-		cw.document.write(html);
-		cw.document.close();
+		verticalAlignToCssProp: function(va) {
+			var value = null;
+			if ('top' === va) {
+				value = 'flex-start';
+			} else if ('center' === va) {
+				value = 'center';
+			} else if ('bottom' === va) {
+				value = 'flex-end';
+			}
+			return value ? 'align-items:'+value+';' : '';
+		},
 		
-		Ext.defer(function() {
-			cw.focus();
-			cw.print();
-			Ext.fly(ifId).destroy();
-		}, 500);
+		horizontalAlignToCssProp: function(va) {
+			var value = null;
+			if ('left' === va) {
+				value = 'start';
+			} else if ('center' === va) {
+				value = 'center';
+			} else if ('right' === va) {
+				value = 'end';
+			}
+			return value ? 'justify-content:'+value+';' : '';
+		}
 	}
 });
