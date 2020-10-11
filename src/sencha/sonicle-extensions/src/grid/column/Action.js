3034dc0d955bf31533290cbf2e66f781c9218861
Matteo Albinola <malbinola@sonicle.com>
2020-10-11 15:24:04 +0200

    Fix action column auto-resize

--- src/sencha/sonicle-extensions/src/grid/column/Action.js
+++ src/sencha/sonicle-extensions/src/grid/column/Action.js
@@ -17,9 +17,49 @@ Ext.define('Sonicle.grid.column.Action', {
 		var me = this,
 				items = cfg.items || me.items || [me],
 				width = cfg.width || me.width;
-		if (Ext.isArray(items) && !width) {
-			cfg.width = (10 + (items.length * 20) + 10);
-		}
+		//if (Ext.isArray(items) && !width) {
+		//	cfg.width = (10 + (items.length * 16) + 10);
+		//}
 		me.callParent([cfg]);
+		
+		if (Ext.isArray(items) && !width) {
+			me.on('afterrender', function() {
+				me.setWidth(10 + items.length * me.measureActionWidth() + 10);
+			}, me, {single: true});
+		}
+	},
+	
+	privates: {
+		measureActionWidth: function() {
+			var me = this,
+					view = me.getView(),
+					sto = me.getView().getStore(),
+					html, width;
+			
+			if (sto) {
+				try {
+					html = me.defaultRenderer(null, {}, sto.createModel({}), -1, -1, sto, view);
+				} catch (e) {}
+				if (html) {
+					var ctxId = Ext.id(null, 'so-actioncolumn-dummy-'),
+							ctxEl = Ext.getBody().appendChild({
+								id: ctxId,
+								tag: 'div',
+								style: 'visibility:hidden;pointer-events:none;border:none;',
+								html: html
+							}),
+							act0El;
+					
+					act0El = ctxEl.down('.x-action-col-0');
+					if (act0El) {
+						width = act0El.getWidth(true) + act0El.getMargin('lr') + act0El.getPadding('lr');
+					}
+					Ext.defer(function() {
+						Ext.fly(ctxId).destroy();
+					}, 10);
+				}
+			}
+			return width || 16;
+		}
 	}
 });
