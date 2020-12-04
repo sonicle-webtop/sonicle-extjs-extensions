a9d5f56da1a1ea2b94dd50398fee0beec7a235a8
Matteo Albinola <malbinola@sonicle.com>
2020-12-04 17:24:42 +0100

    Add Open and Copy triggers

---
+++ src/sencha/sonicle-extensions/src/form/trigger/Copy.js
@@ -0,0 +1,36 @@
+/*
+ * Sonicle ExtJs UX
+ * Copyright (C) 2020 Sonicle S.r.l.
+ * sonicle@sonicle.com
+ * http://www.sonicle.com
+ */
+Ext.define('Sonicle.form.trigger.Copy', {
+	extend: 'Ext.form.trigger.Trigger',
+	alias: 'trigger.socopy',
+	uses: [
+		'Sonicle.ClipboardMgr'
+	],
+	
+	/**
+	 * @cfg {Boolean} [copyEmptyValue=true]
+	 * Set to `false` to skip clipboard copy if value is empty. Default to `true`.
+	 */
+	copyEmptyValue: true,
+	
+	/**
+     * @event copy
+     * Fires when the underlying component input field's value has been copied using this trigger.
+	 * @param {Ext.form.field.Text} this
+     */
+	
+	cls: 'so-' + 'form-copy-trigger',
+	extraCls: 'fa fa-clone',
+
+	handler: function(cmp) {
+		var val = cmp.getValue();
+		if (this.copyEmptyValue || !Ext.isEmpty(val)) {
+			Sonicle.ClipboardMgr.copy(val);
+			cmp.fireEvent('copy', cmp);
+		}
+	}
+});
---
+++ src/sencha/sonicle-extensions/src/form/trigger/Open.js
@@ -0,0 +1,50 @@
+/*
+ * Sonicle ExtJs UX
+ * Copyright (C) 2020 Sonicle S.r.l.
+ * sonicle@sonicle.com
+ * http://www.sonicle.com
+ */
+Ext.define('Sonicle.form.trigger.Open', {
+	extend: 'Sonicle.form.trigger.Hideable',
+	alias: 'trigger.soopen',
+	uses: [
+		'Sonicle.URLMgr'
+	],
+	
+	hideOn: 'empty',
+	hideWhenMouseOut: false,
+	
+	/**
+     * @event beforeopen
+     * Fires when the underlying component input field's value has been  using this trigger.
+	 * @param {Ext.form.field.Text} this
+     */
+	
+	/**
+     * @event open
+     * Fires when the underlying component input field's value has been opened using this trigger.
+	 * @param {Ext.form.field.Text} this
+     */
+	
+	cls: 'so-' + 'form-open-trigger',
+	extraCls: 'fa fa-external-link',
+
+	handler: function(cmp) {
+		var val = cmp.getValue();
+		if (cmp.fireEvent('beforeopen', cmp) !== false) {
+			if (Sonicle.String.startsWith(val, 'http', true)) {
+				Sonicle.URLMgr.open(val, true);
+				cmp.fireEvent('open', cmp);
+			}
+		}
+	},
+	
+	isTriggerNeeded: function() {
+		var me = this;
+		if (!me.callParent()) {
+			return false;
+		} else {
+			return Sonicle.String.startsWith(me.field.getValue(), 'http', true);
+		}
+	}
+});
