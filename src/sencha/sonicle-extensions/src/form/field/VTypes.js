e951a58e67be644c9ac4fbb51880e3fa289d884e
Matteo Albinola <malbinola@sonicle.com>
2021-01-26 17:53:00 +0100

    Remove minlen test from complex password vtype

--- src/sencha/sonicle-extensions/src/form/field/VTypes.js
+++ src/sencha/sonicle-extensions/src/form/field/VTypes.js
@@ -13,26 +13,18 @@ Ext.define('Sonicle.form.field.VTypes', {
 	usernameRe: /^[a-z][a-z0-9\.\-\_]+$/i,
 	usernameText: 'This should be a valid username',
 	
-	simplePassword: function(v) {
-		return this.simplePasswordRe.test(v);
-	},
-	simplePasswordRe: /^[\s\S]{1,128}$/,
-	simplePasswordText: 'Password must be at least 1 character long',
-	
 	complexPassword: function(v) {
 		var me = this, count = 0;
-		if(me.complexPasswordLenRe.test(v)) {
-			if(me.complexPasswordLwLRe.test(v)) count++;
-			if(me.complexPasswordUpLRe.test(v)) count++;
-			if(me.complexPasswordNumRe.test(v)) count++;
-			if(me.complexPasswordSpeRe.test(v)) count++;
-		}
+		if (me.complexPasswordLwLRe.test(v)) count++;
+		if (me.complexPasswordUpLRe.test(v)) count++;
+		if (me.complexPasswordNumRe.test(v)) count++;
+		if (me.complexPasswordSpeRe.test(v)) count++;
 		return (count >= 3);
 	},
-	complexPasswordLenRe: /^[\s\S]{8,128}$/,
+	//complexPasswordLenRe: /^[\s\S]{8,128}$/,
 	complexPasswordLwLRe: /.*[a-z].*/,
 	complexPasswordUpLRe: /.*[A-Z].*/,
 	complexPasswordNumRe: /.*[0-9].*/,
 	complexPasswordSpeRe: /.*[^a-zA-Z0-9].*/,
-	complexPasswordText: 'Password must length at least 8 characters and must meet at least three of the following four categories: uppercase latin letters, lowercase latin letters, numbers, special characters'
+	complexPasswordText: 'Password must at least three of the following four categories: uppercase latin letters, lowercase latin letters, numbers, special characters'
 });
