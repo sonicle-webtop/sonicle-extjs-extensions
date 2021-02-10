5f6eaa9a65e666e675b15fb4fca2e0f01d451350
Matteo Albinola <malbinola@sonicle.com>
2021-02-10 17:47:09 +0100

    Improve autoWidth computation

--- src/sencha/sonicle-extensions/src/grid/column/Action.js
+++ src/sencha/sonicle-extensions/src/grid/column/Action.js
@@ -13,6 +13,19 @@ Ext.define('Sonicle.grid.column.Action', {
 	groupable: false,
 	align: 'center',
 	
+	/**
+	 * True to calculate width automatically based on action item's count.
+	 * Set to a number to arrange the with on a fixed number of items.
+	 * @param {Boolean|Number} [autoWidth=true]
+	 */
+	autoWidth: true,
+	
+	/**
+	 * Reserved space to add before and after when calculating column width. 
+	 * @param {Number} [itemsPadding=10]
+	 */
+	itemsPadding: 5,
+	
 	constructor: function(cfg) {
 		var me = this,
 				items = cfg.items || me.items || [me],
@@ -24,7 +37,7 @@ Ext.define('Sonicle.grid.column.Action', {
 		
 		if (Ext.isArray(items) && !width) {
 			me.on('afterrender', function() {
-				me.setWidth(10 + items.length * me.measureActionWidth() + 10);
+				me.setWidth(me.itemsPadding * 2 + ((me.autoWidth !== true && Ext.isNumber(me.autoWidth)) ? me.autoWidth : items.length) * me.measureActionWidth());
 			}, me, {single: true});
 		}
 	},
@@ -41,6 +54,14 @@ Ext.define('Sonicle.grid.column.Action', {
 					html = me.defaultRenderer(null, {}, sto.createModel({}), -1, -1, sto, view);
 				} catch (e) {}
 				if (html) {
+					// If the first action is hidden dinamically programmatically, wrong measures
+					// are returned on the dummy element. Make sure to disarm here any visibility 
+					// modification in order to get the real width.
+					if (html.indexOf('style') === -1) {
+						html = html.replace('></', ' style="display:inline-block !important;"></');
+					} else {
+						html = html.replace('style="', 'style="display:inline-block !important;');
+					}
 					var ctxId = Ext.id(null, 'so-actioncolumn-dummy-'),
 							ctxEl = Ext.getBody().appendChild({
 								id: ctxId,
