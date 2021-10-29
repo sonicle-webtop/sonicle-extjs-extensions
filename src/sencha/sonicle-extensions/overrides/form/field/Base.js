d6cb00880237bf4c88a2306f9ec4fe25c99b41ea
Matteo Albinola <malbinola@sonicle.com>
2021-10-29 16:48:27 +0200

    Update binding when validation fails

---
+++ src/sencha/sonicle-extensions/overrides/form/field/Base.js
@@ -0,0 +1,21 @@
+/**
+ * Override default Ext.form.field.Base
+ * - ensure binded model being updated: the original implementation do not 
+ *  publish the  value if there are any validation errors (see {@link Ext.form.field.Field#getErrors getErrors}).
+ *  You can check this by adding a presence validator on binded field, if you 
+ *  select all text of a textarea and then press CANC or BACKSPACE, you will see 
+ *  that field become red with no value but model's field raw value remain as it 
+ *  was with an outdated value.
+ */
+Ext.define('Sonicle.overrides.form.field.Base', {
+	override: 'Ext.form.field.Base',
+	
+	publishValue: function() {
+		var me = this;
+		if (me.rendered && me.getValidationField()) {
+			me.publishState('value', me.getValue());
+		} else {
+			me.callParent();
+		}
+	}
+});
