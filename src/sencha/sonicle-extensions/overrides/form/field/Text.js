a6954d5ac30f19b25a52735b0c213d8cbf5e3552
Matteo Albinola <malbinola@sonicle.com>
2023-04-04 10:16:10 +0200

    Fix trigger position support (avoid el undefined)

--- src/sencha/sonicle-extensions/overrides/form/field/Text.js
+++ src/sencha/sonicle-extensions/overrides/form/field/Text.js
@@ -5,77 +5,24 @@
 Ext.define('Sonicle.overrides.form.field.Text', {
 	override: 'Ext.form.field.Text',
 	
-	preSubTpl: [
-		'<div id="{cmpId}-triggerWrap" data-ref="triggerWrap"',
-				'<tpl if="ariaEl == \'triggerWrap\'">',
-					'<tpl foreach="ariaElAttributes"> {$}="{.}"</tpl>',
-				'<tpl else>',
-					' role="presentation"',
-				'</tpl>',
-				' class="{triggerWrapCls} {triggerWrapCls}-{ui}">',
-			'<tpl for="triggers">{[this.renderTrigger("left", values, parent)]}</tpl>',
-			'<div id={cmpId}-inputWrap data-ref="inputWrap"',
-				' role="presentation" class="{inputWrapCls} {inputWrapCls}-{ui}">',
-		{
-			renderTrigger: function(position, values, parent) {
-				return (position === values.position) ? values.renderTrigger(parent) : '';
+	onRender: function() {
+		var me = this,
+			triggers = me.getTriggers();
+		
+		me.callParent();
+		if (triggers) {
+			var els = me.triggerWrap.query('.' + Ext.baseCSSPrefix + 'form-trigger'), i;
+			for (i=0; i<els.length; i++) {
+				Ext.iterate(triggers, function(name, trigger) {
+					if (els[i].id === trigger.domId) {
+						if (trigger.position === 'left') {
+							me.inputWrap.insertSibling(els[i], 'before', false);
+						}
+					}
+				});
 			}
 		}
-    ],
-	
-	postSubTpl: [
-			'<tpl if="!Ext.supports.Placeholder">',
-			'<label id="{cmpId}-placeholderLabel" data-ref="placeholderLabel" for="{id}" class="{placeholderCoverCls} {placeholderCoverCls}-{ui}">{placeholder}</label>',
-			'</tpl>',
-			'</div>', // end inputWrap
-			'<tpl for="triggers">{[this.renderTrigger("right", values, parent)]}</tpl>',
-		'</div>', // end triggerWrap
-		{
-			renderTrigger: function(position, values, parent) {
-				return (Ext.isEmpty(values.position) || (position === values.position)) ? values.renderTrigger(parent) : '';
-			}
-		}
-    ],
-	
-	/*
-	.x-form-trigger-wrap > .x-form-trigger:first-child {
-	border-left: 1px solid #b5b8c8;
-}
-	*/
-   
-   /*
-	* On 6.2 this code was to keep caret position on focus
-	* instead of always going to the end
-	*/
-   /*onFocus: function(e) {
-        var me = this,
-            len;
- 
-		//avoid parent code
-        me.callSuper([e]);
-        
-        // This handler may be called when the focus has already shifted to another element;
-        // calling inputEl.select() will forcibly focus again it which in turn might set up
-        // a nasty circular race condition if focusEl !== inputEl.
-        Ext.asap(function() {
-            // This ensures the carret will be at the end of the input element
-            // while tabbing between editors.
-            if (!me.destroyed && document.activeElement === me.inputEl.dom) {
-                len = me.inputEl.dom.value.length;
-				//ext 6.2 : this is the modified code
-                if (me.selectOnFocus) me.selectText(0, len);
-            }
-        });
- 
-        if (me.emptyText) {
-            me.autoSize();
-        }
- 
-        me.addCls(me.fieldFocusCls);
-        me.triggerWrap.addCls(me.triggerWrapFocusCls);
-        me.inputWrap.addCls(me.inputWrapFocusCls);
-        me.invokeTriggers('onFieldFocus', [e]);
-    },*/
+	},
 	
 	/**
 	 * Gets the current inputEl cursor position.
@@ -153,4 +100,38 @@ Ext.define('Sonicle.overrides.form.field.Text', {
 		}
 		return undefined;
 	}
+	
+	 /*
+	* On 6.2 this code was to keep caret position on focus
+	* instead of always going to the end
+	*/
+   /*onFocus: function(e) {
+        var me = this,
+            len;
+ 
+		//avoid parent code
+        me.callSuper([e]);
+        
+        // This handler may be called when the focus has already shifted to another element;
+        // calling inputEl.select() will forcibly focus again it which in turn might set up
+        // a nasty circular race condition if focusEl !== inputEl.
+        Ext.asap(function() {
+            // This ensures the carret will be at the end of the input element
+            // while tabbing between editors.
+            if (!me.destroyed && document.activeElement === me.inputEl.dom) {
+                len = me.inputEl.dom.value.length;
+				//ext 6.2 : this is the modified code
+                if (me.selectOnFocus) me.selectText(0, len);
+            }
+        });
+ 
+        if (me.emptyText) {
+            me.autoSize();
+        }
+ 
+        me.addCls(me.fieldFocusCls);
+        me.triggerWrap.addCls(me.triggerWrapFocusCls);
+        me.inputWrap.addCls(me.inputWrapFocusCls);
+        me.invokeTriggers('onFieldFocus', [e]);
+    },*/
 });
