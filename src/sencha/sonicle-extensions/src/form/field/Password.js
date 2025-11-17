5b2c9f938a4ad8c64597483315b85d6cd36a4b39
malbinola <malbinola@sonicle.com>
2025-11-17 18:34:07 +0100

    Revemp password field WT-1520

--- src/sencha/sonicle-extensions/src/form/field/Password.js
+++ src/sencha/sonicle-extensions/src/form/field/Password.js
@@ -1,50 +1,67 @@
 /*
  * Sonicle ExtJs UX
- * Copyright (C) 2015 Sonicle S.r.l.
- * sonicle@sonicle.com
- * http://www.sonicle.com
+ * Copyright (C) 2025 Sonicle S.r.l.
+ * malbinola[at]sonicle.com
+ * https://www.sonicle.com
  */
 Ext.define('Sonicle.form.field.Password', {
 	extend:'Ext.form.field.Text',
 	alias: 'widget.sopasswordfield',
-	
-	config: {
-		eyeTriggerCls: 'so-passwordfield-trigger-eye',
-		eyeTriggerClsOn: 'far fa-eye-slash',
-		eyeTriggerClsOff: 'far fa-eye'
-	},
+	requires: [
+		'Sonicle.Utils'
+	],
 	
 	inputType: 'password',
-	eye: false,
+	
+	config: {
+		showPassword: false
+	},
+	
+	eyeTriggerCls: 'so-passwordfield-trigger-eye',
+	eyeTriggerClsOn: 'far fa-eye-slash',
+	eyeTriggerClsOff: 'far fa-eye',
 	
 	constructor: function(cfg) {
-		var me = this, 
-				icfg = Ext.apply({}, cfg, me.getInitialConfig()), 
-				triggers = {};
+		var me = this,
+			SoU = Sonicle.Utils,
+			icfg = SoU.getConstructorConfigs2(this, cfg, ['triggers', 'eyeTriggerCls', 'eyeTriggerClsOff']);
 		
-		triggers = Ext.apply(triggers, {
+		cfg.triggers = SoU.mergeTriggers(icfg.triggers, {
 			eye: {
 				weight: -1,
 				cls: icfg.eyeTriggerCls + ' ' + icfg.eyeTriggerClsOff,
-				handler: me.onEyeClick
+				handler: function(s) {
+					me.setShowPassword(!me.showPassword);
+				}
 			}
 		});
-		Ext.apply(cfg, {triggers: triggers});
 		me.callParent([cfg]);
 	},
 	
-	onEyeClick: function(s) {
-		var me = this,
+	afterRender: function() {
+		var me = this;
+		me.callParent(arguments);
+		me.setShowPassword(me.showPassword);
+	},
+	
+	updateShowPassword: function(nv, ov) {
+		this.refreshShowPassword(nv);
+	},
+	
+	privates: {
+		refreshShowPassword: function(show) {
+			var me = this,
 				onCls = me.eyeTriggerClsOn,
 				offCls = me.eyeTriggerClsOff,
-				tri = me.triggers['eye'],
-				tel = tri ? tri.getEl() : null;
-		me.eye = !me.eye;
-		if(tri) {
-			tel.removeCls(onCls);
-			tel.removeCls(offCls);
-			tel.addCls(me.eye ? onCls : offCls);
+				trigger = me.getTrigger('eye'),
+				tel;
+			
+			if (trigger && (tel = trigger.getEl())) {
+				tel.removeCls(onCls);
+				tel.removeCls(offCls);
+				tel.addCls(show ? onCls : offCls);
+			}
+			if (me.inputEl) me.inputEl.set({type: show ? 'text' : 'password'});
 		}
-		if(me.inputEl) me.inputEl.set({type: me.eye ? 'text' : 'password'});
 	}
 });
