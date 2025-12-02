f8dc11cbdf449527bf7f5e354e5e65c6bd7bd468
malbinola <malbinola@sonicle.com>
2025-12-02 16:55:10 +0100

    Update scroller override

--- src/sencha/sonicle-extensions/overrides/scroll/Scroller.js
+++ src/sencha/sonicle-extensions/overrides/scroll/Scroller.js
@@ -14,6 +14,16 @@ Ext.define('Sonicle.scroll.Scroller', {
 		 * to activate both horizontal and vertical tracking.
 		 */
 		trackPosition: false
+		
+		/*
+		verticalBeginSelector: undefined,
+		
+		verticalEndSelector: undefined,
+		
+		horizontalBeginSelector: undefined,
+		
+		horizontalEndSelector: undefined
+		*/
 	},
 	
 	privates: {
@@ -52,19 +62,83 @@ Ext.define('Sonicle.scroll.Scroller', {
 			direction = direction || '';
 			if (scrollEl && !scrollEl.destroyed) {
 				var me = this,
+					/*
+					lookupEl = function(rootEl, selector) {
+						if (rootEl && Ext.isString(selector)) {
+							return Ext.fly(Ext.dom.Query.selectNode(selector, rootEl.dom));
+						} else {
+							return rootEl;
+						}
+					},
+					*/
+					addOrRemoveCls = function(el, addOrRemove, forceRemove) {
+						if (el) {
+							addOrRemove = Ext.Array.from(addOrRemove);
+							for (var i=0; i<addOrRemove.length; i++) {
+								el[forceRemove === true || addOrRemove[i][0] === false ? 'removeCls' : 'addCls'](addOrRemove[i][1]);
+							}
+						}
+					},
 					vertical = direction.indexOf('v') !== -1,
-					horizontal = direction.indexOf('h') !== -1;
+					horizontal = direction.indexOf('h') !== -1,
+					lower, upper;
 				
+				addOrRemoveCls(scrollEl, [vertical, baseCls + '-track-y']);
 				if (vertical) {
-					scrollEl[me.isScrollPosAtBoundary('lower', pos.y, 0, 5) ? 'addCls' : 'removeCls'](baseCls + '-y-at-start');
-					scrollEl[me.isScrollPosAtBoundary('upper', pos.y, maxPos.y, 5) ? 'addCls' : 'removeCls'](baseCls + '-y-at-end');
+					lower = me.isScrollPosAtBoundary('lower', pos.y, 0, 5);
+					upper = me.isScrollPosAtBoundary('upper', pos.y, maxPos.y, 5);
+					addOrRemoveCls(scrollEl, [[lower, baseCls + '-y-pos-begin'], [!lower && !upper, baseCls + '-y-pos-mid'], [upper, baseCls + '-y-pos-end']], lower && upper);
 				}
+				addOrRemoveCls(scrollEl, [horizontal, baseCls + '-track-x']);
 				if (horizontal) {
-					scrollEl[me.isScrollPosAtBoundary('lower', pos.x, 0, 5) ? 'addCls' : 'removeCls'](baseCls + '-x-at-start');
-					scrollEl[me.isScrollPosAtBoundary('upper', pos.x, maxPos.x, 5) ? 'addCls' : 'removeCls'](baseCls + '-x-at-end');
+					lower = me.isScrollPosAtBoundary('lower', pos.x, 0, 5);
+					upper = me.isScrollPosAtBoundary('upper', pos.x, maxPos.x, 5);
+					addOrRemoveCls(scrollEl, [[lower, baseCls + '-x-pos-begin'], [!lower && !upper, baseCls + '-x-pos-mid'], [upper, baseCls + '-x-pos-end']], lower && upper);
 				}
 			}
 		},
+		
+		/*
+		refreshScrollCls_withSelector: function(direction, pos, maxPos, scrollEl, baseCls) {
+			direction = direction || '';
+			if (scrollEl && !scrollEl.destroyed) {
+				var me = this,
+					lookupEl = function(rootEl, selector) {
+						if (rootEl && Ext.isString(selector)) {
+							return Ext.fly(Ext.dom.Query.selectNode(selector, rootEl.dom));
+						} else {
+							return rootEl;
+						}
+					},
+					addOrRemoveCls = function(el, addOrRemove, forceRemove) {
+						if (el) {
+							addOrRemove = Ext.Array.from(addOrRemove);
+							for (var i=0; i<addOrRemove.length; i++) {
+								el[forceRemove === true || addOrRemove[i][0] === false ? 'removeCls' : 'addCls'](addOrRemove[i][1]);
+							}
+						}
+					},
+					vertical = direction.indexOf('v') !== -1,
+					horizontal = direction.indexOf('h') !== -1,
+					lower, upper;
+				
+				addOrRemoveCls(scrollEl, [vertical, baseCls + '-track-y']);
+				if (vertical) {
+					lower = me.isScrollPosAtBoundary('lower', pos.y, 0, 5);
+					upper = me.isScrollPosAtBoundary('upper', pos.y, maxPos.y, 5);
+					addOrRemoveCls(lookupEl(scrollEl, me.getVerticalBeginSelector()), [[lower, baseCls + '-y-pos-begin'], [!lower && !upper, baseCls + '-y-pos-mid']], lower && upper);
+					addOrRemoveCls(lookupEl(scrollEl, me.getVerticalEndSelector()), [[upper, baseCls + '-y-pos-end'], [!lower && !upper, baseCls + '-y-pos-mid']], lower && upper);
+				}
+				addOrRemoveCls(scrollEl, [horizontal, baseCls + '-track-x']);
+				if (horizontal) {
+					lower = me.isScrollPosAtBoundary('lower', pos.x, 0, 5);
+					upper = me.isScrollPosAtBoundary('upper', pos.x, maxPos.x, 5);
+					addOrRemoveCls(lookupEl(scrollEl, me.getVerticalBeginSelector()), [[lower, baseCls + '-x-pos-begin'], [!lower && !upper, baseCls + '-x-pos-mid']], lower && upper);
+					addOrRemoveCls(lookupEl(scrollEl, me.getVerticalEndSelector()), [[upper, baseCls + '-x-pos-end'], [!lower && !upper, baseCls + '-x-pos-mid']], lower && upper);
+				}
+			}
+		},
+		*/
 		
 		isScrollPosAtBoundary: function(type, currentPos, boundaryPos, pxThreshold) {
 			if (Ext.isNumber(currentPos) && Ext.isNumber(boundaryPos)) {
